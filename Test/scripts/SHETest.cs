e4fab583dee971817954b2ef8f9518e5ff46f731
Stuart <cuthbertio24@gmail.com>
2020-06-02 11:44:04 +0100

    Renaming Page Object classes and adding ability to test Chrome and Firefox

--- Test/scripts/SHETest.cs
+++ Test/scripts/SHETest.cs
@@ -13,7 +13,6 @@ namespace SHETest
 {
     class SHETest
     {
- 
         readonly string loginEmail = "d1474341@urhen.com";
         readonly string loginPassword = "Password";
         readonly string LoginButtonLocator = "//a[@class='login']";
@@ -33,14 +32,104 @@ namespace SHETest
         readonly string summerDressesDescLocator = "//div[@class='cat_desc']//span[(contains(.,'Summer Dresses'))]";
         readonly string deleteItemFromBasketLocator = "//a[@title='Delete']";
         readonly string logoutButtonLocator = "//a[@class='logout']";
-
-
-        IWebDriver driver = new FirefoxDriver();
-
- 
+        
         [Test]
-        public void Test()
+        public void FirefoxTest()
         {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+            driver.Url = "http://automationpractice.com/index.php";
+
+            //login to site 
+            IWebElement loginButton = driver.FindElement(By.XPath(LoginButtonLocator));
+            loginButton.Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            IWebElement submitLoginButton = driver.FindElement(By.XPath(SubmitLoginButtonLocator));
+            wait.Until(ExpectedConditions.ElementToBeClickable(submitLoginButton));
+            IWebElement loginEmailForm = driver.FindElement(By.XPath(loginEmailFormLocator));
+            IWebElement loginPassForm = driver.FindElement(By.XPath(loginPassFormLocator));
+            loginEmailForm.SendKeys(loginEmail);
+            loginPassForm.SendKeys(loginPassword);
+            submitLoginButton.Click();
+            Assert.IsTrue(driver.FindElement(By.XPath(accountInfoLocator)).Displayed);
+
+            //select Dresses->Summer Dresses
+            IWebElement dressesCategoryLink = driver.FindElement(By.XPath(dressesCategoryLocator));
+            wait.Until(ExpectedConditions.ElementToBeClickable(dressesCategoryLink));
+            dressesCategoryLink.Click();
+            IWebElement summerDressesCategoryLink = driver.FindElement(By.XPath(summerDressesCategoryLocator));
+            summerDressesCategoryLink.Click();
+            Assert.IsTrue(driver.FindElement(By.XPath(summerDressesDescLocator)).Displayed);
+
+            //Quick-View a dress
+            IWebElement firstDress = driver.FindElements(By.XPath(dressImageLocator))[0];
+            Actions firstAction = new Actions(driver);
+            firstAction.MoveToElement(firstDress).Build().Perform();
+            IWebElement dressQuickView = driver.FindElement(By.XPath(dressQuickViewLocator));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(dressQuickViewLocator)));
+            dressQuickView.Click();
+            IWebElement iframeElement = driver.FindElement(By.XPath(iframeLocator));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(iframeLocator)));
+            driver.SwitchTo().Frame(iframeElement);
+            Assert.IsTrue(driver.FindElement(By.XPath(addToCartButtonLocator)).Displayed);
+
+            //add dress to cart
+            IWebElement addToCartButton = driver.FindElement(By.XPath(addToCartButtonLocator));
+            addToCartButton.Click();
+            driver.SwitchTo().DefaultContent();
+            wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.XPath(iframeLocator)));
+            Assert.IsTrue(driver.FindElement(By.XPath(addedToCartSuccessfulLocator)).Displayed);
+
+            //add another dress to cart
+            IWebElement continueShoppingButton = driver.FindElement(By.XPath(continueShoppingButtonLocator));
+            continueShoppingButton.Click();
+            IWebElement secondDress = driver.FindElements(By.XPath(dressImageLocator))[2];
+            wait.Until(ExpectedConditions.ElementToBeClickable(secondDress));
+            Actions secondAction = new Actions(driver);
+            secondAction.MoveToElement(secondDress).Build().Perform();
+            IWebElement secondDressQuickView = driver.FindElement(By.XPath(dressQuickViewLocator));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(dressQuickViewLocator)));
+            secondDressQuickView.Click();
+            IWebElement newiFrameElement = driver.FindElement(By.XPath(iframeLocator));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(iframeLocator)));
+            driver.SwitchTo().Frame(newiFrameElement);
+            Assert.IsTrue(driver.FindElement(By.XPath(addToCartButtonLocator)).Displayed);
+            IWebElement newAddToCartButton = driver.FindElement(By.XPath(addToCartButtonLocator));
+            newAddToCartButton.Click();
+            driver.SwitchTo().DefaultContent();
+            wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.XPath(iframeLocator)));
+            Assert.IsTrue(driver.FindElement(By.XPath(addedToCartSuccessfulLocator)).Displayed);
+
+            //go to checkout
+            IWebElement checkoutButton = driver.FindElement(By.XPath(checkoutButtonLocator));
+            checkoutButton.Click();
+
+            //verify correct dress is selected on summary page
+            Assert.IsTrue(driver.FindElement(By.XPath("//small[contains(text(),'SKU : demo_5')]")).Displayed);
+            Assert.IsTrue(driver.FindElement(By.XPath("//small[contains(text(),'SKU : demo_7')]")).Displayed);
+
+            //remove 2nd dress from cart
+            IWebElement deleteSecondDress = driver.FindElements(By.XPath(deleteItemFromBasketLocator))[1];
+            deleteSecondDress.Click();
+            wait.Until(ExpectedConditions.InvisibilityOfElementLocated(By.XPath("//small[contains(text(),'SKU : demo_7')]")));
+            IList<IWebElement> basketList = driver.FindElements(By.XPath(deleteItemFromBasketLocator));
+            int basketSize = basketList.Count();
+            Assert.IsTrue(basketSize == 1);
+            Assert.IsTrue(driver.FindElement(By.XPath("//small[contains(text(),'SKU : demo_5')]")).Displayed);
+
+            //sign out
+            IWebElement logoutButton = driver.FindElement(By.XPath(logoutButtonLocator));
+            logoutButton.Click();
+            Assert.IsTrue(driver.FindElement(By.XPath(LoginButtonLocator)).Displayed);
+
+            driver.Close();
+        }
+
+        [Test]
+        public void ChromeTest()
+        {
+            IWebDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
             driver.Url = "http://automationpractice.com/index.php";
